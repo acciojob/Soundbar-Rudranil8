2bbb35b6e513dcf45cadfe85b85f9aec6c628934
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-19 22:31:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-const buttons = document.querySelectorAll('.btn');
-const stopButton = document.querySelector('.stop');
-const sounds = [];
+// const buttons = document.querySelectorAll('.btn');
+// const stopButton = document.querySelector('.stop');
+// const sounds = [];
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const sound = new Audio(`sounds/${button.innerText}.mp3`);
-        sounds.push(sound);
-        sound.play();
-    });
-});
+// buttons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const sound = new Audio(`sounds/${button.innerText}.mp3`);
+//         sounds.push(sound);
+//         sound.play();
+//     });
+// });
 
-stopButton.addEventListener('click', () => {
-    sounds.forEach(sound => {
-        sound.pause();
-        sound.currentTime = 0;
+// stopButton.addEventListener('click', () => {
+//     sounds.forEach(sound => {
+//         sound.pause();
+//         sound.currentTime = 0;
+//     });
+// });
+window.onload = function() {
+    const buttons = document.querySelectorAll('.btn');
+    const stopButton = document.querySelector('.stop');
+    const sounds = [];
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const sound = new Audio(`sounds/${button.innerText}.mp3`);
+            sounds.push(sound);
+            sound.play();
+        });
     });
-});
+
+    stopButton.addEventListener('click', () => {
+        sounds.forEach(sound => {
+            sound.pause();
+            sound.currentTime = 0;
+        });
+    });
+};
