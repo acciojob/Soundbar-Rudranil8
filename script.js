e9456741e308cd4f45a0e3ffdbcfeaf0abe43e24
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-19 12:04:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const sound = new Audio(`sounds/${button.innerText}.mp3`);
+sound.play();
+sounds.forEach(sound => sound.pause());
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+const sounds = [];
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const sound = new Audio(`sounds/${button.innerText}.mp3`);
+        sounds.push(sound);
+        sound.play();
+    });
+});
+
+stopButton.addEventListener('click', () => {
+    sounds.forEach(sound => sound.pause());
+});
