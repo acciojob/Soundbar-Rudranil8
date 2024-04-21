19b3d1b9da45f3859b1c547e5c6f52c4b6e287a2
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-21 21:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,20 +19,27 @@
 window.onload = function() {
     const buttons = document.querySelectorAll('.btn');
     const stopButton = document.querySelector('.stop');
-    const sounds = [];
+    let sound;
 
     buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const sound = new Audio(`sounds/${button.innerText}.mp3`);
-            sounds.push(sound);
+        // button.addEventListener('click', () => {
+        //     const sound = new Audio(`sounds/${button.innerText}.mp3`);
+        //     sounds.push(sound);
+        //     sound.play();
+        // });
+		button.addEventListener('click', () => {
+            if (sound) {
+                sound.pause();
+                sound.currentTime = 0;
+            }
+            sound = new Audio(`sounds/${button.innerText}.mp3`);
             sound.play();
         });
     });
-
-    stopButton.addEventListener('click', () => {
-        sounds.forEach(sound => {
+	stopButton.addEventListener('click', () => {
+        if (sound) {
             sound.pause();
             sound.currentTime = 0;
-        });
+        }
     });
 };
